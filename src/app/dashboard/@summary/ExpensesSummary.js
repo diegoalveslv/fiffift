0ae219984fab1e0ef00e19f7697ccc3267c270cc
Diego Silva <diego.alveslv@gmail.com>
2024-07-01 15:14:56 -0300

    refac to improve encapsulation

---
+++ src/app/dashboard/@summary/ExpensesSummary.js
@@ -0,0 +1,249 @@
+import { NumericFormatProps } from "@/components/NumericFormatCustomInput";
+import { TableCell, TableRow, Typography } from "@mui/material";
+import { NumericFormat } from "react-number-format";
+
+export class ExpensesSummary {
+  constructor(incomeIdealDivision, expensesAverage) {
+    this.incomeIdealDivision = incomeIdealDivision;
+    this.essentialsExpenses = expensesAverage.filter(
+      (expense) => expense.type === "ESSENTIALS"
+    );
+    this.investmentExpenses = expensesAverage.filter(
+      (expense) => expense.type === "INVESTMENT"
+    );
+    this.leisureExpenses = expensesAverage.filter(
+      (expense) => expense.type === "LEISURE"
+    );
+  }
+
+  makeHeaders() {
+    return [
+      {
+        id: "essentials",
+        header: "Essentials 50%",
+        subtitle: (
+          <NumericFormat
+            value={this.incomeIdealDivision.essentials}
+            displayType="text"
+            {...NumericFormatProps}
+          />
+        ),
+      },
+      {
+        id: "investment",
+        header: "Investment 15%",
+        subtitle: (
+          <NumericFormat
+            value={this.incomeIdealDivision.investment}
+            displayType="text"
+            {...NumericFormatProps}
+          />
+        ),
+      },
+      {
+        id: "leisure",
+        header: "Leisure 35%",
+        subtitle: (
+          <NumericFormat
+            value={this.incomeIdealDivision.leisure}
+            displayType="text"
+            {...NumericFormatProps}
+          />
+        ),
+      },
+    ];
+  }
+
+  makeExpensesTableRows() {
+    const maxRows = Math.max(
+      this.essentialsExpenses.length,
+      this.investmentExpenses.length,
+      this.leisureExpenses.length
+    );
+
+    const rows = [];
+    for (let i = 0; i < maxRows; i++) {
+      const essentialsExpense = this.essentialsExpenses[i];
+      const investmentExpense = this.investmentExpenses[i];
+      const leisureExpense = this.leisureExpenses[i];
+
+      const cells = (
+        <>
+          <TableCell>{essentialsExpense?.description}</TableCell>
+          <TableCell>
+            <NumericFormat
+              value={essentialsExpense?.averageAmount}
+              displayType="text"
+              {...NumericFormatProps}
+            />
+          </TableCell>
+          <TableCell>{investmentExpense?.description}</TableCell>
+          <TableCell>
+            <NumericFormat
+              value={investmentExpense?.averageAmount}
+              displayType="text"
+              {...NumericFormatProps}
+            />
+          </TableCell>
+          <TableCell>{leisureExpense?.description}</TableCell>
+          <TableCell>
+            <NumericFormat
+              value={leisureExpense?.averageAmount}
+              displayType="text"
+              {...NumericFormatProps}
+            />
+          </TableCell>
+        </>
+      );
+
+      const row = <TableRow key={`row${i}`}>{cells}</TableRow>;
+
+      rows.push(row);
+    }
+
+    return rows;
+  }
+
+  calculateTotalsPerType() {
+    const essentialsTotal = this.essentialsExpenses.reduce(
+      (total, expense) => total + expense.averageAmount,
+      0
+    );
+    const investmentTotal = this.investmentExpenses.reduce(
+      (total, expense) => total + expense.averageAmount,
+      0
+    );
+    const leisureTotal = this.leisureExpenses.reduce(
+      (total, expense) => total + expense.averageAmount,
+      0
+    );
+
+    return [essentialsTotal, investmentTotal, leisureTotal];
+  }
+
+  makeTotalPerTypeTableRow() {
+    const [essentialsTotal, investmentTotal, leisureTotal] =
+      this.calculateTotalsPerType();
+
+    return (
+      <TableRow>
+        <TableCell>
+          <Typography>Total</Typography>
+        </TableCell>
+        <TableCell>
+          <NumericFormat
+            value={essentialsTotal}
+            displayType="text"
+            {...NumericFormatProps}
+          />
+        </TableCell>
+        <TableCell></TableCell>
+        <TableCell>
+          <NumericFormat
+            value={investmentTotal}
+            displayType="text"
+            {...NumericFormatProps}
+          />
+        </TableCell>
+        <TableCell></TableCell>
+        <TableCell>
+          <NumericFormat
+            value={leisureTotal}
+            displayType="text"
+            {...NumericFormatProps}
+          />
+        </TableCell>
+      </TableRow>
+    );
+  }
+
+  makeDifferencePerTypeTableRow() {
+    const diffEssentials =
+      this.incomeIdealDivision.essentials -
+      this.essentialsExpenses.reduce(
+        (acc, expense) => expense.averageAmount + acc,
+        0
+      );
+    const diffInvestment =
+      this.incomeIdealDivision.investment -
+      this.investmentExpenses.reduce(
+        (acc, expense) => expense.averageAmount + acc,
+        0
+      );
+    const diffLeisure =
+      this.incomeIdealDivision.leisure -
+      this.leisureExpenses.reduce(
+        (acc, expense) => expense.averageAmount + acc,
+        0
+      );
+
+    const customNumericProps = { ...NumericFormatProps, allowNegative: true };
+
+    return (
+      <TableRow>
+        <TableCell>
+          <Typography>Difference</Typography>
+        </TableCell>
+        <TableCell>
+          <NumericFormat
+            value={diffEssentials}
+            displayType="text"
+            {...customNumericProps}
+          />
+        </TableCell>
+        <TableCell></TableCell>
+        <TableCell>
+          <NumericFormat
+            value={diffInvestment}
+            displayType="text"
+            {...customNumericProps}
+          />
+        </TableCell>
+        <TableCell></TableCell>
+        <TableCell>
+          <NumericFormat
+            value={diffLeisure}
+            displayType="text"
+            {...customNumericProps}
+          />
+        </TableCell>
+      </TableRow>
+    );
+  }
+
+  makeTotalAndAmountLeftTableRow() {
+    const [essentialsTotal, investmentTotal, leisureTotal] =
+      this.calculateTotalsPerType();
+
+    const totalExpenses = essentialsTotal + investmentTotal + leisureTotal;
+    const amountLeft =
+      totalExpenses -
+      (this.incomeIdealDivision.essentials +
+        this.incomeIdealDivision.investment +
+        this.incomeIdealDivision.leisure);
+    return (
+      <TableRow>
+        <TableCell colSpan={3}>
+          <Typography>
+            Total expenses:{" "}
+            <NumericFormat
+              value={totalExpenses}
+              displayType="text"
+              {...NumericFormatProps}
+            />
+          </Typography>
+        </TableCell>
+        <TableCell colSpan={3}>
+          <Typography>
+            AmountLeft:{" "}
+            <NumericFormat
+              value={amountLeft}
+              displayType="text"
+              {...NumericFormatProps}
+            />
+          </Typography>
+        </TableCell>
+      </TableRow>
+    );
+  }
+}
--- src/app/dashboard/@summary/SummaryGrid.js
+++ src/app/dashboard/@summary/SummaryGrid.js
@@ -1,5 +1,4 @@
 "use client";
-import { NumericFormatProps } from "@/components/NumericFormatCustomInput";
 import {
   Table,
   TableBody,
@@ -9,80 +8,25 @@ import {
   TableRow,
   Typography,
 } from "@mui/material";
-import { NumericFormat } from "react-number-format";
+import { ExpensesSummary } from "./ExpensesSummary";
 
 export default function SummaryGrid({ incomeIdealDivision, expensesAverage }) {
-  const headerColumns = [
-    {
-      id: "essentials",
-      header: "Essentials 50%",
-      subtitle: (
-        <NumericFormat
-          value={incomeIdealDivision.essentials}
-          displayType="text"
-          {...NumericFormatProps}
-        />
-      ),
-    },
-    {
-      id: "investment",
-      header: "Investment 15%",
-      subtitle: (
-        <NumericFormat
-          value={incomeIdealDivision.investment}
-          displayType="text"
-          {...NumericFormatProps}
-        />
-      ),
-    },
-    {
-      id: "leisure",
-      header: "Leisure 35%",
-      subtitle: (
-        <NumericFormat
-          value={incomeIdealDivision.leisure}
-          displayType="text"
-          {...NumericFormatProps}
-        />
-      ),
-    },
-  ];
-
-  const essentialsExpenses = expensesAverage.filter(
-    (expense) => expense.type === "ESSENTIALS"
-  );
-  const investmentExpenses = expensesAverage.filter(
-    (expense) => expense.type === "INVESTMENT"
-  );
-  const leisureExpenses = expensesAverage.filter(
-    (expense) => expense.type === "LEISURE"
-  );
-
-  const expensesTableRows = makeExpensesTableRows(
-    essentialsExpenses,
-    investmentExpenses,
-    leisureExpenses
-  );
-
-  const totalPerTypeTableRow = makeTotalPerTypeTableRow(
-    essentialsExpenses,
-    investmentExpenses,
-    leisureExpenses
-  );
-
-  const differencePerTypeTableRow = makeDifferencePerTypeTableRow(
+  const expensesSummary = new ExpensesSummary(
     incomeIdealDivision,
-    essentialsExpenses,
-    investmentExpenses,
-    leisureExpenses
+    expensesAverage
   );
 
-  // const totalAndAmountLeftTableRow = makeTotalAndAmountLeftTableRow(
-  //   incomeIdealDivision,
-  //   essentialsExpenses,
-  //   investmentExpenses,
-  //   leisureExpenses
-  // );
+  const headerColumns = expensesSummary.makeHeaders();
+
+  const expensesTableRows = expensesSummary.makeExpensesTableRows();
+
+  const totalPerTypeTableRow = expensesSummary.makeTotalPerTypeTableRow();
+
+  const differencePerTypeTableRow =
+    expensesSummary.makeDifferencePerTypeTableRow();
+
+  const totalAndAmountLeftTableRow =
+    expensesSummary.makeTotalAndAmountLeftTableRow();
 
   return (
     <>
@@ -102,172 +46,10 @@ export default function SummaryGrid({ incomeIdealDivision, expensesAverage }) {
             {expensesTableRows}
             {totalPerTypeTableRow}
             {differencePerTypeTableRow}
-            <TableRow>
-              <TableCell colSpan={3}>Total expenses: R$</TableCell>
-              <TableCell colSpan={3}>Amount left: R$</TableCell>
-            </TableRow>
+            {totalAndAmountLeftTableRow}
           </TableBody>
         </Table>
       </TableContainer>
     </>
   );
-}
-
-function makeExpensesTableRows(
-  essentialsExpenses,
-  investmentExpenses,
-  leisureExpenses
-) {
-  const maxRows = Math.max(
-    essentialsExpenses.length,
-    investmentExpenses.length,
-    leisureExpenses.length
-  );
-
-  const rows = [];
-  for (let i = 0; i < maxRows; i++) {
-    const essentialsExpense = essentialsExpenses[i];
-    const investmentExpense = investmentExpenses[i];
-    const leisureExpense = leisureExpenses[i];
-
-    const cells = (
-      <>
-        <TableCell>{essentialsExpense?.description}</TableCell>
-        <TableCell>
-          <NumericFormat
-            value={essentialsExpense?.averageAmount}
-            displayType="text"
-            {...NumericFormatProps}
-          />
-        </TableCell>
-        <TableCell>{investmentExpense?.description}</TableCell>
-        <TableCell>
-          <NumericFormat
-            value={investmentExpense?.averageAmount}
-            displayType="text"
-            {...NumericFormatProps}
-          />
-        </TableCell>
-        <TableCell>{leisureExpense?.description}</TableCell>
-        <TableCell>
-          <NumericFormat
-            value={leisureExpense?.averageAmount}
-            displayType="text"
-            {...NumericFormatProps}
-          />
-        </TableCell>
-      </>
-    );
-
-    const row = <TableRow key={`row${i}`}>{cells}</TableRow>;
-
-    rows.push(row);
-  }
-
-  return rows;
-}
-
-function makeTotalPerTypeTableRow(
-  essentialsExpenses,
-  investmentExpenses,
-  leisureExpenses
-) {
-  const essentialsTotal = essentialsExpenses.reduce(
-    (total, expense) => total + expense.averageAmount,
-    0
-  );
-  const investmentTotal = investmentExpenses.reduce(
-    (total, expense) => total + expense.averageAmount,
-    0
-  );
-  const leisureTotal = leisureExpenses.reduce(
-    (total, expense) => total + expense.averageAmount,
-    0
-  );
-
-  return (
-    <TableRow>
-      <TableCell>Total</TableCell>
-      <TableCell>
-        <NumericFormat
-          value={essentialsTotal}
-          displayType="text"
-          {...NumericFormatProps}
-        />
-      </TableCell>
-      <TableCell>Total</TableCell>
-      <TableCell>
-        <NumericFormat
-          value={investmentTotal}
-          displayType="text"
-          {...NumericFormatProps}
-        />
-      </TableCell>
-      <TableCell>Total</TableCell>
-      <TableCell>
-        <NumericFormat
-          value={leisureTotal}
-          displayType="text"
-          {...NumericFormatProps}
-        />
-      </TableCell>
-    </TableRow>
-  );
-}
-
-function makeDifferencePerTypeTableRow(
-  incomeIdealDivision,
-  essentialsExpenses,
-  investmentExpenses,
-  leisureExpenses
-) {
-  const diffEssentials =
-    incomeIdealDivision.essentials -
-    essentialsExpenses.reduce((acc, expense) => expense.averageAmount + acc, 0);
-  const diffInvestment =
-    incomeIdealDivision.investment -
-    investmentExpenses.reduce((acc, expense) => expense.averageAmount + acc, 0);
-  const diffLeisure =
-    incomeIdealDivision.leisure -
-    leisureExpenses.reduce((acc, expense) => expense.averageAmount + acc, 0);
-
-  const customNumericProps = { ...NumericFormatProps, allowNegative: true };
-
-  return (
-    <TableRow>
-      <TableCell>Difference</TableCell>
-      <TableCell>
-        <NumericFormat
-          value={diffEssentials}
-          displayType="text"
-          {...customNumericProps}
-        />
-      </TableCell>
-      <TableCell>Difference</TableCell>
-      <TableCell>
-        <NumericFormat
-          value={diffInvestment}
-          displayType="text"
-          {...customNumericProps}
-        />
-      </TableCell>
-      <TableCell>Difference</TableCell>
-      <TableCell>
-        <NumericFormat
-          value={diffLeisure}
-          displayType="text"
-          {...customNumericProps}
-        />
-      </TableCell>
-    </TableRow>
-  );
-}
-
-function makeTotalAndAmountLeftTableRow(
-  incomeIdealDivision,
-  essentialsExpenses,
-  investmentExpenses,
-  leisureExpenses
-) {
-  return <TableRow></TableRow>;
 }
